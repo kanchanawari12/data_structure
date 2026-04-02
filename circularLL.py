cdaddee446371244a1e1dfdb0731d990ee32d98f
kanchanawari12 <kanchanawari3@gmail.com>
2026-04-02 15:32:07 +0530

    ADD_END method completed

--- circularLL.py
+++ circularLL.py
@@ -34,10 +34,22 @@ class Linklist:
             n.ref=self.head
         
     def add_end(self,data):
-        pass
-
+        new_node=Node(data)
+        if self.head is None:
+            new_node.ref=new_node
+            self.head=new_node
+            return
+        else:
+            n=self.head
+            while n.ref != self.head:
+                n=n.ref
+            n.ref=new_node
+            new_node.ref=self.head
+            
 a1=Linklist()
 a1.add_begin(10)
 a1.add_begin(10)
 a1.add_begin(30)
+a1.add_end(40)
+a1.add_end(50)
 a1.traversal()
