72b714891ac9d90cf814e0414811f5f4eb923785
kanchanawari12 <kanchanawari3@gmail.com>
2026-04-02 18:13:08 +0530

    BST file added

---
+++ binary-search-tree.py
@@ -0,0 +1,11 @@
+class BST:
+    def __init__(self,data):
+        self.lchild=None
+        self.data=data
+        self.rchild=None
+
+
+root=BST(10)
+print(root.data)
+print(root.lchild)
+print(root.rchild)
