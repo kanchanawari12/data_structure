54b11205aca691f4c30db5c495c13fc8a43b9166
kanchanawari12 <kanchanawari3@gmail.com>
2026-04-06 18:19:46 +0530

    search method completed

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -22,8 +22,29 @@ class BST:
             else:
                 self.rchild.insert(data)
 
+
+    def search(self,data):
+        if self.key is None:
+            print("Tree is Empty")
+        if self.key == data:
+            print("found")
+            return
+        if self.key > data:
+            if self.lchild is None:
+                print("Not found")
+                return
+            else:
+                self.lchild.search(data)
+        else:
+            if self.rchild is None:
+                print("Not found")
+                return
+            else:
+                self.rchild.search(data)
+
 root=BST(None)
 root.insert(10)
 list=[1,5,15,20,3]
 for i in list:
     root.insert(i)
+root.search(21)
